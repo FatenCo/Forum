10ae01a334afa4b2977d177c565ff3dbdf387180
faten mekni <fatenmekni092@gmail.com>
2024-01-09 02:30:33 -1100

    Partie backend de Forum/

---
+++ Models/Answer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Forum.Models
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        //relation
+        public string? IdentityUserId { get; set; }
+        [ForeignKey("IdentityUserId")]
+        public IdentityUser? User { get; set; }
+        public int? QuestionId { get; set; }
+        [ForeignKey("QuestionId")]
+        public Question? Question { get; set; }
+    }
+}
